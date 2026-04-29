ca0f5ada0718493a349ac2b142f13cba4e614c0d
Vernoxy-Media <vernoxy4@gmail.com>
2026-04-29 15:02:52 +0530

    Update firebase config

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,38 +1,17 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDRZXLhxX9Bpa5INYqGG0RSFnKCbyCKfXQ",
-//   authDomain: "fit2feb.firebaseapp.com",
-//   projectId: "fit2feb",
-//   storageBucket: "fit2feb.firebasestorage.app",
-//   messagingSenderId: "933900684413",
-//   appId: "1:933900684413:web:6e21645cc9900b2aceb514"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-// export default app;
-
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; // ← Add this
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCRu5eU22MoG1uJi56n5wnfnVZhTaR5WI",
-  authDomain: "fib2fab-3d3e0.firebaseapp.com",
-  projectId: "fib2fab-3d3e0",
-  storageBucket: "fib2fab-3d3e0.firebasestorage.app",
-  messagingSenderId: "103833128120",
-  appId: "1:103833128120:web:5edfcd10038d0e4b19f84d",
-  measurementId: "G-9Z0LREK3N0",
+  apiKey: "AIzaSyDRZXLhxX9Bpa5INYqGG0RSFnKCbyCKfXQ",
+  authDomain: "fit2feb.firebaseapp.com",
+  projectId: "fit2feb",
+  storageBucket: "fit2feb.firebasestorage.app",
+  messagingSenderId: "933900684413",
+  appId: "1:933900684413:web:6e21645cc9900b2aceb514",
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const auth = getAuth(app); // ← Add this
 export default app;
